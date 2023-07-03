5f16f42a8e6b0b0bc38570b26bf10cb88a496c40
bibek-in-github <mahatcoding@gmail.com>
2023-07-03 07:47:59 -0700

    added bottom navigation bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -55,26 +55,36 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverPadding(
             padding: const EdgeInsets.only(top: 20.0),
             sliver: SliverToBoxAdapter(
-              child: Previews(title: 'Previews', contentList: previews),
+              child: Previews(
+                key: PageStorageKey('previews'),
+                title: 'Previews',
+                contentList: previews,
+              ),
             ),
           ),
           SliverToBoxAdapter(
             child: ContentList(
+              key: PageStorageKey('myList'),
               title: 'My List',
               contentList: myList,
             ),
           ),
           SliverToBoxAdapter(
             child: ContentList(
+              key: PageStorageKey('originals'),
               title: 'Netflix Originals',
               contentList: originals,
               isOriginals: true,
             ),
           ),
-          SliverToBoxAdapter(
-            child: ContentList(
-              title: 'Trending ',
-              contentList: trending,
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+              child: ContentList(
+                key: PageStorageKey('trending'),
+                title: 'Trending ',
+                contentList: trending,
+              ),
             ),
           ),
         ],
